95f13c86b4449e2177f31e450fdd89c1f9117b23
GibreelAbdullah <gibreel.khan@gmail.com>
2023-09-18 22:06:33 +0530

    Updates

--- _app/immutable/chunks/paths.9fb6e971.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_18unb88)==null?void 0:s.base)??"/anamscape";var a;const t=((a=globalThis.__sveltekit_18unb88)==null?void 0:a.assets)??e;export{t as a,e as b};

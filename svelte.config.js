8fd18ac802fe2ddb013e74762e05dde2898e55de
GibreelAbdullah <gibreel.khan@gmail.com>
2023-09-19 10:43:59 +0530

    Removed Paths

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        },
 		prerender: {
 			entries: ['/blogs/A_Broken_Toy.html', '/blogs/Anam\'s_Nascent_Poetry:_A_Book_Review.html']
 		}

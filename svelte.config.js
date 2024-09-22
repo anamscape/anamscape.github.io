40f62979aebc021483aa8384f2c2514c69e0d42e
Gibreel Abdullah <gibreel.khan@gmail.com>
2024-09-22 18:04:22 +0530

    Added prerender links

--- svelte.config.js
+++ svelte.config.js
@@ -8,14 +8,34 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
-			entries: ['/blogs/A_Broken_Toy.html', '/blogs/Anam\'s_Nascent_Poetry:_A_Book_Review.html']
+			entries: ["/blogs/Existential Diaries: A Book Review",
+				"/blogs/Dewa Mela 2018: Strolling down memory lane",
+				"/blogs/Struggles Unveiled: On Getting Published",
+				"/blogs/हर पल बदलती ज़िंदगी",
+				"/blogs/Writers on Writing",
+				"/blogs/Under the Radiant Sky",
+				"/blogs/An Exclusive Self-talk with Anam Fatima, Author of Nascent Poetry",
+				"/blogs/Anam's Nascent Poetry: A Book Review",
+				"/blogs/Book Review: The Struggle Between My Heart & My Brain by Mehr Bani",
+				"/blogs/Discovering the Poetic Horizons: In Conversation with Yaseen Anwer",
+				"/blogs/Let's Walk Along the Memory Lane (Part 1)",
+				"/blogs/Just Another Day",
+				"/blogs/Snow Love",
+				"/blogs/A Younger Sister’s Story",
+				"/blogs/Rohan and a Squirrel",
+				"/blogs/A Broken Toy",
+				"/blogs/Book Review: The Shadow of the Dark Soul by Sukhmani Gandhi",
+				"/blogs/Channelizing Anger into Creativity",
+				"/blogs/Photocapturasis Syndrome",
+				"/blogs/Are You Free",
+				"/blogs/Why Literature"]
 		}
 	}
 };
